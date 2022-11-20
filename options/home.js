7df27f03f1839abadea902d02cf6eea357d681d8
shawn hui <shawnhui88@gmail.com>
2022-11-20 00:58:03 -0800

    add notes on how to set up js files

--- options/home.js
+++ options/home.js
@@ -1,5 +1,16 @@
 /**
  * check bookmark list empty 
+ * if empty 
+ * show empty home page content
+ * 
+ * if not empty
+ * show filled home page content
  * populate this weeks bookmarks
  * n x 3 box
+ */
+
+/** Different JS file and export functions into home.js and library.js
+ * pinning functionality 
+ * menu functionality 
+ * card setup 
  */
